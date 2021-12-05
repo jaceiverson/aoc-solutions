a84c87a32b3f6e6197fa38012fa605c5fa7ce98f
Jace Iverson <iverson.jace@gmail.com>
2021-12-05 16:25:01 -0500

    added answer print statements, black formating compliant

--- solutions/helper.py
+++ solutions/helper.py
@@ -8,10 +8,23 @@ def read(path):
 
 
 def create_file(day):
+    """
+    Will create a python file will all the lines of code I normally use
+    1. Doc String with URL to today's challenge
+    2. import statement to get the file reader
+    3. code line to read the input
+    4. add the part1 & part2 answer strings
+
+    From the main directory run:
+        $ python solutions/helper.py {day int to create}
+
+    """
     file_text = (
         f'"""https://adventofcode.com/2021/day/{day}"""\n\n'
         f"from helper import read\n\n"
-        f'data = read("./inputs/{day}.txt")\n\n'
+        f'data = read("./inputs/{day}.txt")\n\n\n'
+        'print(f"PART 1: {part_1_answer}")\n\n'
+        'print(f"PART 2: {part_2_answer}")\n'
     )
 
     with open(f"./solutions/day{day}.py", "a") as f:
