8d781c75ea6f74a0fc162d15effb0161fc014bde
Jace Iverson <iverson.jace@gmail.com>
2021-12-11 02:00:09 -0500

    black formated d11 answer

--- 2021/solutions/day11.py
+++ 2021/solutions/day11.py
@@ -7,18 +7,18 @@ import copy
 # READ INPUT
 data = read("./2021/inputs/11.txt")
 # TEST INPUT
-#data = read("./2021/inputs/11-test.txt")
+# data = read("./2021/inputs/11-test.txt")
 # PARSE INPUT
-data = data.strip().split('\n')
-data = [[int(x) for x in y]for y in data]
+data = data.strip().split("\n")
+data = [[int(x) for x in y] for y in data]
 
 
 class Oct:
-    def __init__(self,grid) -> None:
+    def __init__(self, grid) -> None:
         self.grid = grid
         self.flashes = 0
 
-    def set_grid(self,grid):
+    def set_grid(self, grid):
         self.grid = grid
 
     def show(self):
@@ -26,7 +26,7 @@ class Oct:
             print(row)
 
     def reset_flash_count(self):
-        self.flashes = 0 
+        self.flashes = 0
 
     def zero_check(self):
         """Will return True if all are 0"""
@@ -36,9 +36,9 @@ class Oct:
                     return False
         return True
 
-    def process(self,step):
-        #print(step)
-        #self.show()
+    def process(self, step):
+        # print(step)
+        # self.show()
         self.increase_1()
         self.reset_flashed_grid()
 
@@ -47,27 +47,28 @@ class Oct:
         while not self.zero_check():
             self.process(counter)
             counter += 1
-        
+
         return counter
 
     def increase_1(self):
         # increase all ints by 1 - GOOD
-        for r_idx,row in enumerate(self.grid):
-            for c_idx,col in enumerate(row):
+        for r_idx, row in enumerate(self.grid):
+            for c_idx, col in enumerate(row):
                 if self.grid[r_idx][c_idx] < 10:
                     self.grid[r_idx][c_idx] += 1
                     if self.grid[r_idx][c_idx] > 9:
                         self.flashes += 1
-                        self.increase_neighbors(r_idx,c_idx)
+                        self.increase_neighbors(r_idx, c_idx)
 
-    def increase_neighbors(self,row,col):
-        for row_offset in range(-1,2):
-            for col_offset in range(-1,2):
+    def increase_neighbors(self, row, col):
+        for row_offset in range(-1, 2):
+            for col_offset in range(-1, 2):
                 # not self
                 if not (row_offset == 0 and col_offset == 0):
                     # not out of bounds
                     if (0 <= row + row_offset < len(self.grid)) and (
-                        0 <= col + col_offset < len(self.grid[0]) ):
+                        0 <= col + col_offset < len(self.grid[0])
+                    ):
                         # if the cell is below 10 increase it
                         if self.grid[row + row_offset][col + col_offset] < 10:
                             # increase neighbors
@@ -75,11 +76,13 @@ class Oct:
                             # will flash if cell checking is > 9
                             if self.grid[row + row_offset][col + col_offset] > 9:
                                 self.flashes += 1
-                                self.increase_neighbors(row + row_offset,col + col_offset)
-    
+                                self.increase_neighbors(
+                                    row + row_offset, col + col_offset
+                                )
+
     def reset_flashed_grid(self):
-        for r_idx,row in enumerate(self.grid):
-            for c_idx,col in enumerate(row):
+        for r_idx, row in enumerate(self.grid):
+            for c_idx, col in enumerate(row):
                 if self.grid[r_idx][c_idx] > 9:
                     self.grid[r_idx][c_idx] = 0
 
