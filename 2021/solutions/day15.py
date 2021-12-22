2f0445ca202f08d0e5c5934a0c81cec306b3fa91
Jace Iverson <iverson.jace@gmail.com>
2021-12-21 23:27:00 -0800

    added some rough ideas for 15, need pathfinding algo

---
+++ 2021/solutions/day15.py
@@ -0,0 +1,22 @@
+"""https://adventofcode.com/2021/day/15"""
+
+from helper import read
+
+# READ INPUT
+data = read("./2021/inputs/15.txt")
+# TEST INPUT
+# data = read("./2021/inputs/15-test.txt")
+# PARSE INPUT
+data = data.strip().split('\n')
+
+# PART 1
+# A* ALGORITHM? 
+start = (0,0)
+while start != (len(data[0]),len(data)):
+    pass
+part_1_answer = None
+print(f"PART 1: {part_1_answer}")
+
+# PART 2
+part_2_answer = None
+print(f"PART 2: {part_2_answer}")
