ffc8dc870140b0e9024633685dd7d5f284b6441b
Jace Iverson <iverson.jace@gmail.com>
2021-12-24 14:32:08 -0800

    added wop for day 23

---
+++ 2021/solutions/day23.py
@@ -0,0 +1,32 @@
+"""https://adventofcode.com/2021/day/23"""
+
+from helper import read
+
+"""
+#############
+#...........#
+###B#B#D#A###
+  #D#C#A#C#
+  #########
+
+by hand -> 
+D3 -> U1,R3
+A3 -> U2,L6
+
+
+"""
+# READ INPUT
+data = read("./2021/inputs/23.txt")
+# TEST INPUT
+# data = read("./2021/inputs/23-test.txt")
+# PARSE INPUT
+data = data.strip().split("\n")
+# PART 1
+
+part_1_answer = None
+print(f"PART 1: {part_1_answer}")
+
+# PART 2
+
+part_2_answer = None
+print(f"PART 2: {part_2_answer}")
