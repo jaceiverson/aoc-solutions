9959ccd163440c2d1ddedfa6b8d76cb1833ff8cf
Jace Iverson <jiverson@Jace-Iverson-DC6V2M5FWK.local>
2022-11-09 22:34:51 -0500

    solved 8 and 23 part 1

---
+++ 2017/solutions/day23.py
@@ -0,0 +1,76 @@
+"""https://adventofcode.com/2017/day/23"""
+
+from helper import read
+from string import ascii_lowercase as alphabet
+
+# READ INPUT
+data = read("./2017/inputs/23.txt").strip().split("\n")
+# TEST INPUT
+# data = read("./2017/inputs/23-test.txt").strip().split("\n")
+# PARSE INPUT
+R = {letter: 0 for letter in alphabet[:8]}
+
+
+def get_value(value: str):
+    return R[value] if value in alphabet else int(value)
+
+
+# PART 1
+def part_1(data: list[str]):
+    position = 0
+    mul_count = 0
+    while 0 <= position < len(data):
+        instruction, register, value = data[position].split()
+        if instruction == "set":
+            R[register] = get_value(value)
+            position += 1
+        elif instruction == "sub":
+            R[register] -= get_value(value)
+            position += 1
+        elif instruction == "mul":
+            R[register] *= get_value(value)
+            mul_count += 1
+            position += 1
+        elif instruction == "jnz":
+            if get_value(register) != 0:
+                position += get_value(value) - 1
+            position += 1
+    return mul_count
+
+
+part_1_answer = part_1(data)
+print(f"PART 1: {part_1_answer}")
+
+# PART 2
+# https://www.reddit.com/r/adventofcode/comments/7lms6p/2017_day_23_solutions/
+def part_2(data: list[str]):
+    position = 0
+    passed_positions = []
+    while 0 <= position < len(data):
+        print(position)
+        passed_positions.append(position)
+        instruction, register, value = data[position].split()
+        if instruction == "set":
+            R[register] = get_value(value)
+            position += 1
+        elif instruction == "sub":
+            R[register] -= get_value(value)
+            position += 1
+        elif instruction == "mul":
+            R[register] *= get_value(value)
+            position += 1
+        elif instruction == "jnz":
+            if get_value(register) != 0:
+                position += get_value(value) - 1
+            position += 1
+
+        if position in passed_positions:
+            break
+
+    return R
+
+
+R = {letter: 0 for letter in alphabet[:8]}
+R["a"] = 1
+part_2_answer = part_2(data)
+print(f"PART 2: {part_2_answer}")
---
+++ 2017/solutions/day8.py
@@ -0,0 +1,84 @@
+"""https://adventofcode.com/2017/day/8"""
+
+from helper import read
+
+# READ INPUT
+data = read("./2017/inputs/8.txt").strip().split("\n")
+# TEST INPUT
+# data = read("./2017/inputs/8-test.txt").strip().split("\n")
+# PARSE INPUT
+R = {}
+
+
+def opperation(left: int, right: int, o: str):
+    if o == "<":
+        return left < right
+    elif o == ">":
+        return left > right
+    elif o == "==":
+        return left == right
+    elif o == "!=":
+        return left != right
+    elif o == "<=":
+        return left <= right
+    elif o == ">=":
+        return left >= right
+
+
+def getter(key: str):
+    value = R.get(key)
+    if value is None:
+        R[key] = 0
+        return 0
+    return value
+
+
+def part_1(line: str):
+    reg, moves, number, _if, reg_to_check, comparison, comparison_value = line.split()
+    current_value = getter(reg)
+    # check if the condition is met
+    if opperation(getter(reg_to_check), int(comparison_value), comparison):
+        if moves == "inc":
+            R[reg] += int(number)
+        else:
+            R[reg] -= int(number)
+        # print(R)
+
+
+# PART 1
+for l in data:
+    part_1(l)
+part_1_answer = max(R.values())
+print(f"PART 1: {part_1_answer}")
+
+# PART 2
+def part_2(data):
+    max_value = 0
+    for line in data:
+        (
+            reg,
+            moves,
+            number,
+            _if,
+            reg_to_check,
+            comparison,
+            comparison_value,
+        ) = line.split()
+        current_value = getter(reg)
+        # check if the condition is met
+        if opperation(getter(reg_to_check), int(comparison_value), comparison):
+            if moves == "inc":
+                R[reg] += int(number)
+            else:
+                R[reg] -= int(number)
+
+            if R[reg] > max_value:
+                max_value = R[reg]
+        # print(R)
+    return max_value
+
+
+R = {}
+part_2_answer = part_2(data)
+print(f"PART 2: {part_2_answer}")
+# 10494 -> TO HIGH (forgot to clear R)
