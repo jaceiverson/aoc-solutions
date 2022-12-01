2a0e4fe7a14c2c6070c0fe8f10a553df7d2ccdad
Jace Iverson <jiverson@Jace-Iverson-DC6V2M5FWK.local>
2022-12-01 11:09:45 -0500

    solved part 1 of day 12 with some help

--- 2021/solutions/day12.py
+++ 2021/solutions/day12.py
@@ -31,28 +31,6 @@ if the lower cave has no children, cannot path that way
 leave from start, finish with end
 
 """
-# https://topaz.github.io/paste/#XQAAAQAqAwAAAAAAAAAyGUj/TuRfOI7nwo+gbNPmE8BAa64yJre1LXowmkvToIIzGJwTLCwJ9fWHR92o9Pr7IoNBSXMfZdes5kjMoCgiBdmtAPxECRLmCC7dQl5tIwFkKbAkt0itH5L6wgfDy83JzAdaNjh68Ip4ccx8gPeib0SymvtKDyCPuK3JDtbPYYxe4E37DUHSBJSXeUgSjJqlhLMBmDn0MByD9I3+fIv31Q2lFWOc75Zl0CHnTi0IzmJ5AupkjcOIXLjQWE7yCRg69lA7PicQZUYoxfIOBQzVQN0UBx3yOZ2vK38G89AhJOnwtP3CtV5HOU9lI14c+vZ+F7bOkkqR+bwgj2J4w0d/xtoidFjdnj4v9fMeN0eFo+h6YFzAZ3rRbDHXdzN8JownTCCVtwdKz53jCYj/JsqkAA==
-def countPaths(currentCave, noEntry):
-    if currentCave == "end":
-        # we have arrived at the end of the cave
-        return 1
-    paths = C.get(currentCave)[:]
-    paths = [path for path in paths if path not in noEntry]
-
-    # there is no valid path
-    if not paths:
-        return 0
-    # if keep track of the caves we can't enter
-    if currentCave[0] != currentCave[0].upper():
-        noEntry = noEntry + [currentCave]
-
-    print("Current Cave:", currentCave)
-    print("Paths:", paths)
-    print("Do not Enter:", noEntry)
-    return sum(countPaths(path, noEntry) for path in paths)
-
-
-print(countPaths("start", []))
 
 
 def path(path_options, current_path):
@@ -72,12 +50,69 @@ def path(path_options, current_path):
     return current_path
 
 
-p = path(C, ["start"])
+# https://topaz.github.io/paste/#XQAAAQAqAwAAAAAAAAAyGUj/TuRfOI7nwo+gbNPmE8BAa64yJre1LXowmkvToIIzGJwTLCwJ9fWHR92o9Pr7IoNBSXMfZdes5kjMoCgiBdmtAPxECRLmCC7dQl5tIwFkKbAkt0itH5L6wgfDy83JzAdaNjh68Ip4ccx8gPeib0SymvtKDyCPuK3JDtbPYYxe4E37DUHSBJSXeUgSjJqlhLMBmDn0MByD9I3+fIv31Q2lFWOc75Zl0CHnTi0IzmJ5AupkjcOIXLjQWE7yCRg69lA7PicQZUYoxfIOBQzVQN0UBx3yOZ2vK38G89AhJOnwtP3CtV5HOU9lI14c+vZ+F7bOkkqR+bwgj2J4w0d/xtoidFjdnj4v9fMeN0eFo+h6YFzAZ3rRbDHXdzN8JownTCCVtwdKz53jCYj/JsqkAA==
+def countPaths(currentCave, noEntry):
+    if currentCave == "end":
+        # print("Current Cave:",currentCave)
+        # print("Past Cave:",pastCave)
+        # print("Do not Enter:",noEntry)
+        # print()
+        return 1
+    paths = C.get(currentCave)[:]
+    paths = [path for path in paths if path not in noEntry]
+    # print("Current Cave:",currentCave)
+    # print("Past Cave:",pastCave)
+    # print("Paths:",paths)
+    # print("Do not Enter:",noEntry)
+    # print()
+    if not paths:
+        return 0
+    if currentCave[0] != currentCave[0].upper():
+        noEntry = noEntry + [currentCave]
+    return sum(countPaths(path, noEntry) for path in paths)
+
 
 # PART 1
-
+part_1_answer = countPaths("start", [])
 print(f"PART 1: {part_1_answer}")
 
 # PART 2
+def part_2(currentCave, noEntry, visited_caves, lower_case_limit_reached=False):
+    if currentCave == "end":
+        # print("Current Cave:",currentCave)
+        # print("Past Cave:",pastCave)
+        print(f"Do not Enter: {noEntry}\nVISITED: {visited_caves}\n---\n")
+        return 1
+    if currentCave == "start":
+        visited_caves = ["start"]
+    else:
+        visited_caves += [currentCave]
 
+    paths = C.get(currentCave)[:]
+    paths = [path for path in paths if path not in noEntry]
+    # print("Current Cave:",currentCave)
+    # print("Past Cave:",pastCave)
+    # print("Paths:",paths)
+    # print("Do not Enter:",noEntry)
+    # print()
+    if not paths:
+        return 0
+
+    # see if we have visited any lwoercase cave twice
+    lower_case_limit_reached = any(
+        (visited_caves.count(x) > 1 for x in visited_caves if x == x.lower())
+    )
+
+    # we need to adjust the logic for no entry
+    # cannot return to start
+    if (
+        currentCave[0] != currentCave[0].upper() and lower_case_limit_reached
+    ) or currentCave == "start":
+        noEntry = noEntry + [currentCave]
+    return sum(
+        part_2(path, noEntry, visited_caves, lower_case_limit_reached) for path in paths
+    )
+
+
+part_2_answer = part_2("start", [], [])
 print(f"PART 2: {part_2_answer}")
