50476847fdd533eab49afa4bf855a0d4b79b0699
Jace Iverson <jiverson@Jace-Iverson-DC6V2M5FWK.local>
2022-12-06 00:14:46 -0500

    day 6 in the books

---
+++ 2022/solutions/day6.py
@@ -0,0 +1,38 @@
+"""https://adventofcode.com/2022/day/6"""
+
+from helper import read
+
+# READ INPUT
+data = read("./2022/inputs/6.txt").strip()
+# TEST INPUT
+# data = read("./2022/inputs/6-test.txt").strip().split("\n")
+# PARSE INPUT
+def find_start_character(
+    data: str,
+    consecutive_unique: int,
+    output: bool = False,
+) -> int:
+    sequence_found = False
+    idx = consecutive_unique
+    while not sequence_found:
+        d = data[idx - consecutive_unique : idx]
+        if len(set(d)) == len(d):
+            sequence_found = True
+            if output:
+                print(idx)
+                print(d)
+            return idx
+        idx += 1
+
+
+# PART 1
+part_1_answer = find_start_character(data, 4)
+print(f"PART 1: {part_1_answer}")
+
+# PART 2
+part_2_answer = find_start_character(data, 14)
+print(f"PART 2: {part_2_answer}")
+
+# a little disapointed this one was so slow,
+# I forgot about sets for a while and was trying to do the comparison myself. oops
+# fun problem
