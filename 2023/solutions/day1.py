dd0eeffea606aea386299143e80f4e1fdfa782c8
Jace Iverson <iverson.jace@gmail.com>
2023-11-27 00:05:56 -0700

    :sparkles: Started with 2023
    With the new script struture I also need the TEMPLATE_FILE.py in my root of my project. That is what will create the other files when I use the newday command.

---
+++ 2023/solutions/day1.py
@@ -0,0 +1,19 @@
+"""https://adventofcode.com/2023/day/1"""
+
+from aoc_util.helper import read
+
+# READ INPUT
+data = read("./2023/inputs/1.txt").strip().split("\n")
+# TEST INPUT
+# data = read("./2023/inputs/1-test.txt").strip().split("\n")
+# PARSE INPUT
+
+# PART 1
+
+part_1_answer = None
+print(f"PART 1: {part_1_answer}")
+
+# PART 2
+
+part_2_answer = None
+print(f"PART 2: {part_2_answer}")
