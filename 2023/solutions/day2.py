5bc38c08e250922876540897cfca6db234ed26c4
Jace Iverson <iverson.jace@gmail.com>
2023-12-03 00:04:12 -0700

    :sparkles: Day 2 and 3 solves

---
+++ 2023/solutions/day2.py
@@ -0,0 +1,75 @@
+"""https://adventofcode.com/2023/day/2"""
+
+from aoc_util import read
+
+# READ INPUT
+data = read("./2023/inputs/2.txt").strip().split("\n")
+# TEST INPUT
+# data = read("./2023/inputs/2-test.txt").strip().split("\n")
+# PARSE INPUT
+COLORS = ["red","green","blue"]
+LIMIT = [12,13,14]
+
+def is_valid_pull(pull:str):
+    data = pull.split(", ")
+    for type_ in data:
+        score, color = type_.split(" ")
+        if int(score) > LIMIT[COLORS.index(color)]:
+            return False
+    return True
+
+def process_line(line:str):
+    # total reset for each game
+    pull_results = []
+    # parse the line
+    game_id,results = line.split(": ")
+    _, game_id = game_id.split(" ")
+    pulls = results.split("; ")
+    for p in pulls:
+        pull_results.append(is_valid_pull(p))
+
+    if all(pull_results):
+        return int(game_id)
+
+    return 0
+
+def part_1(data):
+    return sum(process_line(line) for line in data)
+
+
+
+# PART 1
+part_1_answer = part_1(data)
+print(f"PART 1: {part_1_answer}")
+# 231 not right
+
+# PART 2
+def is_valid_pull_part_2(p,maxes):
+    pull_data = p.split(", ")
+    for type_ in pull_data:
+        score, color = type_.split(" ")
+        if int(score) > maxes[COLORS.index(color)]:
+            maxes[COLORS.index(color)] = int(score)
+    return maxes
+
+def process_line_part_2(line:str):
+    # total reset for each game
+    pull_results = []
+    # parse the line
+    game_id,results = line.split(": ")
+    _, game_id = game_id.split(" ")
+    pulls = results.split("; ")
+    maxes = [0,0,0]
+    for p in pulls:
+        maxes = is_valid_pull_part_2(p,maxes)
+
+    return maxes[0] * maxes[1] * maxes[2]
+
+def part_2(data):
+    total = 0
+    for line in data:
+        total += process_line_part_2(line) 
+    return total
+
+part_2_answer = part_2(data)
+print(f"PART 2: {part_2_answer}")
---
+++ 2023/solutions/day3.py
@@ -0,0 +1,110 @@
+"""https://adventofcode.com/2023/day/3"""
+
+from aoc_util import read
+import re
+from rich import print
+
+# READ INPUT
+data = read("./2023/inputs/3.txt").strip().split("\n")
+# TEST INPUT
+# data = read("./2023/inputs/3-test.txt").strip().split("\n")
+# data = read("./2023/inputs/3-test-b.txt").strip().split("\n")
+# PARSE INPUT
+def get_next_number_index(line:str,start:int):
+    #print(f"NN: {line[start:]=}")
+    for x in line[start:]:
+        if x.isnumeric():
+            return start
+        else:
+            start+=1
+    return -1
+
+def make_number_map(data:list):
+    map_ = {}
+    for line_idx, line in enumerate(data):
+        nums = re.findall(r"\d+",line)
+        #print(line_idx)
+        #print(nums)
+        #print(f"{line}\n")
+        last_idx = 0
+        # loop through all the numbers found
+        for n in nums:
+            #print(f"LL: {line[last_idx:]=}")
+            # get the index of the number accounting for duplicates
+            start_idx = line[last_idx:].index(n)+last_idx
+            # save the map
+            for i in range(start_idx,len(n)+start_idx):
+                map_[(line_idx,i)] = n
+            # move the point we are jumping off from
+            last_idx = get_next_number_index(line,len(n)+start_idx)
+
+    return map_
+
+NUMBER_MAP = make_number_map(data)
+# print(NUMBER_MAP)
+
+
+def scan_area(x:int,y:int):
+    """
+    part number touches a non . symbol
+
+    return true if is part number
+    return false if is not part number
+    """
+    numbers = []
+    for x_scan in range(-1,2):
+        for y_scan in range(-1,2):
+            if not x_scan == 0 == y_scan:
+                chr = data[x+x_scan][y+y_scan]
+                # print(chr)
+                if chr.isnumeric():
+                    numbers.append((x+x_scan,y+y_scan))
+    return numbers
+
+
+def extract_numbers(data:list,nums:list):
+    cleaned_numbers = []
+    last_y = None
+    last_x = None
+    for n in nums:
+        x,y= n
+        if x==last_x and y==last_y+1:
+            pass
+        else:
+            # do work
+            cleaned_numbers.append(int(NUMBER_MAP[n]))
+        last_x = x
+        last_y = y
+    return cleaned_numbers
+
+def part_1(data:list):
+    sum_ = 0
+    for line_idx,line in enumerate(data):
+        for idx_,chr in enumerate(line):
+            if not chr.isnumeric() and chr != ".":
+                numbers =  scan_area(line_idx,idx_)
+                # print(numbers)
+                clean_numbers = extract_numbers(data,numbers)
+                for c in clean_numbers:
+                    sum_ += c
+    return sum_
+
+
+# PART 1
+part_1_answer = part_1(data)
+print(f"PART 1: {part_1_answer}")
+
+# PART 2
+def part_2(data:list):
+    sum_ = 0
+    for line_idx,line in enumerate(data):
+        for idx_,chr in enumerate(line):
+            if chr=="*":
+                numbers =  scan_area(line_idx,idx_)
+                clean_numbers = extract_numbers(data,numbers)
+                if len(clean_numbers)==2:
+                    sum_ += clean_numbers[0]*clean_numbers[1]
+    return sum_
+
+part_2_answer = part_2(data)
+print(f"PART 2: {part_2_answer}")
